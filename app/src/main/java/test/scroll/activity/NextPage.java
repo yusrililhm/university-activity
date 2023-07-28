b356c8ba3f84f0c470212e1b638e23732e43113e
yusrililhm <yusrililham62@gmail.com>
2023-07-28 19:12:09 +0700

    belajar activity di android

---
+++ app/src/main/java/test/scroll/activity/NextPage.java
@@ -0,0 +1,14 @@
+package test.scroll.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NextPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_page);
+    }
+}
